ce8616b5a11bcc00907d586c1f5a02e5fd4a0999
h0x91b <h0x91b@gmail.com>
2020-01-01 15:48:55 +0300

    [+] gta.read_memory(ptr, size) [+] gta.write_memory(ptr, bin)

--- MFCLuaDll/LuaGta2Module/dllmain.cpp
+++ MFCLuaDll/LuaGta2Module/dllmain.cpp
@@ -1,19 +1,124 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 #define LUA_LIB
-//#include "lua.hpp"
+
 extern "C" {
-    //#include "lua.h"
-//    #include "lauxlib.h"
-
-#include <lua.h>
-#include <lualib.h>
-#include <lauxlib.h>
-
+    #include <lua.h>
+    #include <lualib.h>
+    #include <lauxlib.h>
 }
 
 #pragma comment(lib, "lua-5.3.5.lib")
-    //#include "lua.hpp"
+
+
+bool IsBadReadPtr(void* p)
+{
+    MEMORY_BASIC_INFORMATION mbi = { 0 };
+    if (::VirtualQuery(p, &mbi, sizeof(mbi)))
+    {
+        DWORD mask = (PAGE_READONLY | PAGE_READWRITE | PAGE_WRITECOPY | PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_WRITECOPY);
+        bool b = !(mbi.Protect & mask);
+        // check the page is not a guard page
+        if (mbi.Protect & (PAGE_GUARD | PAGE_NOACCESS)) b = true;
+
+        return b;
+    }
+    return true;
+}
+
+bool IsBadWritePtr(void* p)
+{
+    MEMORY_BASIC_INFORMATION mbi = { 0 };
+    if (::VirtualQuery(p, &mbi, sizeof(mbi)))
+    {
+        DWORD mask = (PAGE_READWRITE | PAGE_WRITECOPY | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_WRITECOPY);
+        bool b = !(mbi.Protect & mask);
+        // check the page is not a guard page
+        if (mbi.Protect & (PAGE_GUARD | PAGE_NOACCESS)) b = true;
+
+        return b;
+    }
+    return true;
+}
+
+// dest, src
+static int gta_write_memory(lua_State* L) {
+    void *src, *dest;
+    size_t size, size2;
+
+    int argc = lua_gettop(L);
+    if (!argc == 3) {
+        lua_pushliteral(L, "must be 2 arguments");
+        lua_error(L);
+        return 0;
+    }
+
+    if (!lua_isinteger(L, 1)) {
+        lua_pushliteral(L, "1st argument must be an integer (dest)");
+        lua_error(L);
+        return 0;
+    }
+
+    if (!lua_isstring(L, 2)) {
+        lua_pushliteral(L, "2st argument must be a string (src)");
+        lua_error(L);
+        return 0;
+    }
+
+    dest = (void*)lua_tointeger(L, 1);
+    src = (void*)lua_tolstring(L, 2, &size);
+
+    if (IsBadWritePtr(dest)) {
+        lua_pushboolean(L, 0);
+        lua_pushliteral(L, "ptr is invalid");
+        return 2;
+    }
+
+    memcpy(dest, src, size);
+    lua_pushboolean(L, 1);
+    lua_pushlstring(L, (const char*)dest, size);
+
+    return 2;
+}
+
+static int gta_read_memory(lua_State* L) {
+    void* ptr;
+    size_t size;
+
+    int argc = lua_gettop(L);
+    if (!argc == 2) {
+        lua_pushliteral(L, "must be 2 arguments");
+        lua_error(L);
+        return 0;
+    }
+
+    if (!lua_isinteger(L, 1)) {
+        lua_pushliteral(L, "1st argument must be an integer");
+        lua_error(L);
+        return 0;
+    }
+
+    if (!lua_isinteger(L, 2)) {
+        lua_pushliteral(L, "2st argument must be an integer");
+        lua_error(L);
+        return 0;
+    }
+
+    ptr = (void*)lua_tointeger(L, 1);
+    size = (size_t)lua_tointeger(L, 2);
+
+    if (IsBadReadPtr(ptr)) {
+        lua_pushboolean(L, 0);
+        lua_pushliteral(L, "ptr is invalid");
+        return 2;
+    }
+
+
+    lua_pushboolean(L, 1);
+    lua_pushlstring(L, (const char*)ptr, size);
+
+    return 2;
+}
 
 static int gta_hello_world(lua_State* L) {
     lua_pushstring(L, "GTA module hello world");
@@ -22,6 +127,8 @@ static int gta_hello_world(lua_State* L) {
 
 static const luaL_Reg gtalib[] = {
         {"hello",   gta_hello_world},
+        {"read_memory", gta_read_memory},
+        {"write_memory", gta_write_memory},
         {NULL, NULL}
 };
 
