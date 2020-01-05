8779a526b53820e889eaff39754e9f0995a695cd
h0x91b <h0x91b@gmail.com>
2020-01-06 02:23:25 +0300

    [+] api.getGameStatus

--- MFCLuaDll/api.lua
+++ MFCLuaDll/api.lua
@@ -1,5 +1,43 @@
 local api = {}
 local gta = require("gta")
+local ffi = require("ffi")
+
+ffi.cdef[[
+int __cdecl HelloFFI(int, int, int);
+
+typedef enum GAME_STATUS {
+    GAME_PAUSED=2,
+    GAME_RUN=1
+} GAME_STATUS;
+
+typedef struct {
+    enum GAME_STATUS gameStatus;
+    void * slots[6];
+    int alsoCurrentSlot;
+    char field_0x20;
+    char slotIndex;
+    int field_0x24;
+    int field_0x28;
+    int field_0x2c;
+    void * currentSlot;
+} Game;
+
+]]
+
+local pGame = ffi.cast( "Game**", 0x005eb4fc )
+
+function api.getGameStatus()
+	log(tostring(pGame))
+	log(tostring(pGame[0]))
+	if pGame[0] == nil then
+		return 0, "GAME_NOT_STARTED"
+	elseif pGame[0].gameStatus == 1 then
+		return 1, "GAME_RUN"
+	else
+		return 2, "GAME_PAUSED"
+	end
+end
+
 
 function api.GetNextPedId()
 	local r, mem = gta.read_memory(0x00591e84, 4)
